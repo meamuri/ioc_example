3eff70f501f7d4333ef789f285f6767664c0640b
meamuri <meammf@yandex.ru>
2017-11-26 16:47:05 +0300

    musician data class

---
+++ src/main/kotlin/broke_ioc/Application.kt
@@ -0,0 +1,5 @@
+package broke_ioc
+
+fun main(args: Array<String>) {
+
+}
---
+++ src/main/kotlin/broke_ioc/entity/Musician.kt
@@ -0,0 +1,7 @@
+package broke_ioc.entity
+
+/**
+ * https://kotlinlang.org/docs/reference/data-classes.html
+ * data classes documentation
+ */
+data class Musician(val name: String, val instrument: String)
