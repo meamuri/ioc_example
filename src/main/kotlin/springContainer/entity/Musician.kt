f28335e85b1c627901782f07a2b56c75eca0270e
meamuri <meammf@yandex.ru>
2017-11-28 20:16:51 +0300

    data class was changed

--- src/main/kotlin/springContainer/entity/Musician.kt
+++ src/main/kotlin/springContainer/entity/Musician.kt
@@ -1,6 +1,9 @@
 package springContainer.entity
 
 import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
 
 /**
  * https://kotlinlang.org/docs/reference/data-classes.html
@@ -8,6 +11,13 @@ import javax.persistence.Entity
  */
 @Entity
 data class Musician(private val name: String, private val instrument: String) {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    val id: Long? = null
+
+    constructor() : this("", "")
+
     fun soundCheck(): String {
         return "musician $name make noice on $instrument instrument"
     }
